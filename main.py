f73e577ab67d4e42d9115801b8689be19d82edeb
Julio César <julliusrock138@gmail.com>
2023-04-07 10:55:46 -0300

    Subindo as funcionalidades da tela manutenção

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from view import create_main_window
 from exam import create_exam_window
 from manutencao import create_manu_window
 from entrada_dados import create_entrada_window
-from query_list import cadastro_exames
+from query_list import cadastro_exames, pesquisa_id, pesquisa_exame, atualizar_exame
 
 
 
@@ -61,6 +61,68 @@ if __name__ == "__main__":
                 window['-CADASTRO_VALOR-'].update(0.00)
                 window['-CADASTRO_SALDO-'].update(0)
                 
-    
+        elif event == "-VOLTAR_EXAME-":
+            window.close()
+            window = create_main_window()
+        
+        # Usando o event para ultilizar a tela de masnutenção de exames
+        elif '::manutenção_exame' in event:
+            window.close()
+            window = create_manu_window()
+        
+        # Evento utilizado para definir por onde será relizado a pesquisa, se por ID ou por Nome de Exame
+        elif event == "-PESQUISAR_POR_ID-" and values["-PESQUISAR_POR_ID-"] == True:
+            window['-PROCURAR_ID-'].update(readonly=False)
+            window['-MANUTENÇÃO_EXAME-'].update(readonly=True)
+            window['-MANUTENÇÃO_VALOR-'].update(readonly=True)
+            window['-MANUTENÇÃO_SALDO-'].update(readonly=True)
+            window['-PESQUISAR_EXAME-'].update(visible=False)
+            window['-PESQUISAR_ID-'].update(visible=True)
+        
+        elif event == "-PESQUISA_POR_EXAME-" and values["-PESQUISA_POR_EXAME-"] == True:
+            window['-PROCURAR_ID-'].update(readonly=True)
+            window['-MANUTENÇÃO_EXAME-'].update(readonly=False)
+            window['-MANUTENÇÃO_VALOR-'].update(readonly=True)
+            window['-MANUTENÇÃO_SALDO-'].update(readonly=True)
+            window['-PESQUISAR_ID-'].update(visible=False)
+            window['-PESQUISAR_EXAME-'].update(visible=True)
+        
+        # Evento que usa as informações de ID para pesquisar no BD
+        elif event == '-PESQUISAR_ID-':
+            try:
+                id_pesquisa = int(values['-PROCURAR_ID-'])
+            except:
+                id_pesquisa = int(sg.popup_get_text("Digite somente Números INTEIROS sem ponto e virgula:", title="ATENÇÃO!"))
+                window['-PROCURAR_ID-'].update(id_pesquisa)
+            
+            resultado_id = pesquisa_id(id_pesquisa)
+            if resultado_id == None:
+                sg.popup_error('Exame não cadastrado', title="ATENÇÃO!")
+            else:
+                window['-MANUTENÇÃO_EXAME-'].update(resultado_id[1])
+                window['-MANUTENÇÃO_VALOR-'].update(resultado_id[2])
+                window['-MANUTENÇÃO_SALDO-'].update(resultado_id[3])
+                window['-MANUTENÇÃO_EXAME-'].update(readonly=False)
+                window['-MANUTENÇÃO_VALOR-'].update(readonly=False)
+                window['-MANUTENÇÃO_SALDO-'].update(readonly=False)
+        
+        elif event == "-PESQUISAR_EXAME-":
+                
+            exame_pesquisa = str(values['-MANUTENÇÃO_EXAME-']).upper()            
+            resultado_exame = pesquisa_exame(exame_pesquisa)
+            if resultado_exame == None:
+                sg.popup_error('Exame não cadastrado', title="ATENÇÃO!")
+                
+            else:
+                window['-PROCURAR_ID-'].update(resultado_exame[0])
+                window['-MANUTENÇÃO_VALOR-'].update(resultado_exame[2])
+                window['-MANUTENÇÃO_SALDO-'].update(resultado_exame[3])
+                window['-MANUTENÇÃO_VALOR-'].update(readonly=False)
+                window['-MANUTENÇÃO_SALDO-'].update(readonly=False)
+        
+        elif event == '-SALVAR_EXAME-':
+            confirmar = sg.popup_ok_cancel('Tem certeza que deseja alterar as informações?', title="ATENÇÃO!")
+            if confirmar == 'OK':
+                atualizar_exame(values['-MANUTENÇÃO_EXAME-'].upper(), values['-MANUTENÇÃO_VALOR-'].replace(',','.'), values['-MANUTENÇÃO_SALDO-'], values['-PROCURAR_ID-'],)
     # Encerrar a janela
     window.close()
--- query_list.py
+++ query_list.py
@@ -20,3 +20,43 @@ def cadastro_exames(exame, valor, saldo):
                 # Esse é o punico erro que pode acontecer, pois somente não conseguira cadastrar um exame, caso o nome já esteja cadastrado
                 sg.popup_cancel('------ Exame já existe no sistema ------', title="ATENÇÃO!")
                 return False
+
+# Query para realizar as pesuisas dos exames cadastrados, tanto por ID quanto por Nome de exame
+def pesquisa_id(dado_pesquisa):
+    with sqlite3.connect('laboratorio.db') as conexao:
+        with closing(conexao.cursor()) as cursor:
+            
+            cursor.execute(
+                '''
+                    SELECT * FROM controle WHERE id = ?
+                ''',(dado_pesquisa,)
+            )
+            resultado = cursor.fetchone()
+            conexao.commit()
+            return resultado   
+            
+
+def pesquisa_exame(dado_pesquisa):
+    with sqlite3.connect('laboratorio.db') as conexao:
+        with closing(conexao.cursor()) as cursor:
+            
+            cursor.execute(
+                '''
+                    SELECT * FROM controle WHERE item = ?
+                ''',(dado_pesquisa,)
+            )
+            resultado = cursor.fetchone()
+            conexao.commit()
+            return resultado
+            
+# Query para atualizar os cadastros de qualquer exame no BD
+def atualizar_exame(item, valor, saldo, id):
+    valores_para_atualizar = [item, valor, saldo, id]
+    with sqlite3.connect('laboratorio.db') as conexao:
+        with closing(conexao.cursor()) as cursor:
+            try: 
+                cursor.execute('UPDATE controle SET item = ?, valor = ?, saldo = ? WHERE id = ?', valores_para_atualizar)
+                conexao.commit() 
+            except:
+                conexao.rollback()
+                
